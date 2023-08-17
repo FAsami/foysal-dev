6f60561389203d8e8c49586a12abe34ea5c14672
fasami <foysal.developer@gmail.com>
2023-08-17 16:12:42 +0600

    lint: make ' to &apos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
 import Head from 'next/head'
 import tw from 'twin.macro'
-import { Button } from 'antd'
 import React from 'react'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>foysal is here :) </title>
+        <title>foysal </title>
       </Head>
       <main tw="h-screen w-screen bg-[#242B2E] flex items-center justify-center">
         <h1
           tw="text-white text-5xl font-bold tracking-[0.15em]"
           className="animate__animated animate__swing"
         >
-          <span tw="text-2xl">Hi! I'm</span>
+          <span tw="text-2xl">Hi I&apos;m</span>
           <br /> FOYSAL
         </h1>
       </main>
