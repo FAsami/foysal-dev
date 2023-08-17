e4c33d4416995e785661abbb702c787054f42677
fasami <foysal.developer@gmail.com>
2023-08-17 14:07:17 +0600

    bump: upgraded to app route

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Foysal',
+  description: 'Created with Love',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
