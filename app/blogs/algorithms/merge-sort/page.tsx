f71e611cf7234fac5407316f518b770b20e18ef0
fasami <foysal.developer@gmail.com>
2023-08-17 14:37:39 +0600

    chore: fresh start

--- app/blogs/algorithms/merge-sort/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MergeSortAlgorithm = () => {
-  return <div>MergeSortAlgorithm</div>
-}
-
-export default MergeSortAlgorithm
--- app/blogs/data-structure/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const DataStructure = () => {
-  return <div>DataStructure</div>
-}
-
-export default DataStructure
